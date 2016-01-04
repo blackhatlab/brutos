7b4cecda077360d585db3144f901954ee594fca5
BlackHat Lab <back.hat.lab@gmail.com>
2016-01-04 23:56:34 +0300

    setup script

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ print ' '*10 + 'Working Directory: ' + path + ' '*10
 print '='*70
 
 try:
+   os.system ('sudo pip install colorama && sudo pip install paramiko && sudo pip install mechanize')
    os.system ('sudo mv "' + script + '/brutos" /usr/bin')
    os.system ('sudo chmod +x /usr/bin/brutos')
    os.system ('sudo mv "' + script + '/brutos_files" /usr/bin')
